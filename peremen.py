0f83443412cb739361f8a38a7fb698e6464c4019
AlexDedAlex <idbogatoff@gmail.com>
2024-09-15 11:39:01 +0300

    Add files via upload

---
+++ peremen.py
@@ -0,0 +1,4 @@
+name_course = 'Python'
+number_homework_completed = 12
+hours_spent = 1.5
+print(f' Курс: {name_course}'','f' всего задач: {number_homework_completed}'','f' затрачено часов: {hours_spent}'','f' среднее время выполнения: {(hours_spent/number_homework_completed)}')
